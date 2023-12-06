d6d658b6d30eed9024f5c3e9558ca62cd640a437
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-12-06 18:26:57 +0900

    add: badge enum class
    혜택이 많은 순으로 열거하여
    인자로 받을 총혜택금액에 따라 원하는 배지를 받도록 구성함

---
+++ src/main/java/christmas/domain/badge/Badge.java
@@ -0,0 +1,31 @@
+package christmas.domain.badge;
+
+import christmas.view.constant.output.ResultFormat;
+import java.util.Arrays;
+
+public enum Badge {
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("별", 5_000),
+    NONE(ResultFormat.NO_BENEFIT.getMessage(), 0);
+
+    private final String name;
+    private final int lowerBoundInclusive;
+
+    Badge(String name, int lowerBoundInclusive) {
+        this.name = name;
+        this.lowerBoundInclusive = lowerBoundInclusive;
+    }
+
+    public static Badge getBy(int benefitAmount) {
+        return Arrays.stream(values())
+                .filter(badge -> benefitAmount >= badge.lowerBoundInclusive)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 유효하지 않은 총혜택 금액입니다."));
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
