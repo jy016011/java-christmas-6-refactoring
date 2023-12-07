0805555cb55a7abec6db0f19655bf161c97b4f1d
jy016011 <39334789+jy016011@users.noreply.github.com>
2023-12-07 12:35:55 +0900

    refactor: change condition in GiftPromotion and add period field
    할인프로모션 클래스와 구조적 일치성을 갖도록 하기위해 위와 같이 수정함

--- src/main/java/christmas/domain/promotion/GiftPromotion.java
+++ src/main/java/christmas/domain/promotion/GiftPromotion.java
@@ -1,30 +1,40 @@
 package christmas.domain.promotion;
 
 import static christmas.domain.promotion.condition.order.OriginTotalPrice.LIMIT_AMOUNT_OF_GIFT_PROMOTION;
+import static christmas.domain.promotion.condition.order.OriginTotalPrice.MIN_AMOUNT_FOR_APPLICABLE;
 
 import christmas.domain.Order;
 import christmas.domain.VisitingDate;
 import christmas.domain.promotion.condition.date.PromotionPeriod;
 import java.util.function.BiPredicate;
+import java.util.function.Predicate;
 
 public enum GiftPromotion implements Promotion {
     CHAMPAGNE_GIFT(
-            (visitingDate, order) ->
-                    PromotionPeriod.DECEMBER.contains(visitingDate)
-                            && LIMIT_AMOUNT_OF_GIFT_PROMOTION.isApplicablePrice(order),
+            PromotionPeriod.DECEMBER::contains,
+            (visitingDate, order) -> LIMIT_AMOUNT_OF_GIFT_PROMOTION.isApplicablePrice(order),
             Gift.CHAMPAGNE
     );
+
+    private final Predicate<VisitingDate> period;
     private final BiPredicate<VisitingDate, Order> condition;
     private final Gift gift;
 
-    GiftPromotion(BiPredicate<VisitingDate, Order> condition, Gift gift) {
+    GiftPromotion(
+            Predicate<VisitingDate> period,
+            BiPredicate<VisitingDate, Order> condition,
+            Gift gift
+    ) {
+        this.period = period;
         this.condition = condition;
         this.gift = gift;
     }
 
     @Override
     public boolean isApplicable(VisitingDate visitingDate, Order order) {
-        return condition.test(visitingDate, order);
+        return period.test(visitingDate)
+                && condition.test(visitingDate, order)
+                && MIN_AMOUNT_FOR_APPLICABLE.isApplicablePrice(order);
     }
 
     public int getAmount() {
